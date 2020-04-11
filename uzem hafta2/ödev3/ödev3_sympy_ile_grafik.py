f116ccebe6f2e323b815bda8bd433d69bfc8f9a4
Taha Emre UĞUR <44301010+Perneko@users.noreply.github.com>
2020-04-11 15:11:31 +0300

    Create ödev3_sympy_ile_grafik.py

---
+++ uzem hafta2/ödev3/ödev3_sympy_ile_grafik.py
@@ -0,0 +1,12 @@
+import sympy as sym
+from sympy import Symbol
+from sympy import pprint
+%matplotlib notebook
+import sympy.plotting as syp
+x=Symbol('x')#sembol atamaları
+lamda=Symbol('lambda')
+
+p=1-sym.exp(-lamda*x)#Exponential Distribution türkçe adıyla üstel dağılım fonksiyonu
+pprint(p)#Sabit ortalama değişme haddinde ortaya çıkan bağımsız olaylar arasındaki zaman aralığını modelleştirirken bir üstel dağılım doğal olarak ortaya çıkar.
+
+print(syp.plot(p.subs({lamda:1}),(x,0,5),title='gauss distribution'))#sympy sayesinde çizilen grafik
